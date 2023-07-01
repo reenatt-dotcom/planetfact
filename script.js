2c6aa78cb6dc01e2760cae1ec8b938907fee6eef
Reena Thomas <rtthomas@ucsc.edu>
2023-07-01 14:01:06 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleContent(sectionId) {
+    var sections = document.getElementsByClassName("content-section");
+    for (var i = 0; i < sections.length; i++) {
+      sections[i].style.display = "none";
+    }
+    document.getElementById(sectionId).style.display = "block";
+  }
+  
